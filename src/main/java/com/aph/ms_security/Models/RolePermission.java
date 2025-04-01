930444bfa35b9274ae4d517276f0fa86b4e316e7
mrgr24 <mrgr2415@gmail.com>
2025-04-01 00:23:58 -0500

    Arreglo Rol-Permisos

--- src/main/java/com/aph/ms_security/Models/RolePermission.java
+++ src/main/java/com/aph/ms_security/Models/RolePermission.java
@@ -6,8 +6,7 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document
-
+@Document()
 public class RolePermission {
     @Id
     private String _id;
@@ -16,15 +15,27 @@ public class RolePermission {
     private Role role;
     @DBRef
     private Permission permission;
-    public RolePermission(String _id) {
-        this._id = _id;
+
+    public RolePermission() {
     }
 
     public String get_id() {
         return _id;
     }
 
-    public void set_id(String _id) {
-        this._id = _id;
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Permission permission) {
+        this.permission = permission;
     }
 }
